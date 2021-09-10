b290584e99902f4bc6681d1a34b295ec69ded820
Eduardo Juliano <ejuliano25@hotmail.com>
2021-09-09 21:04:45 -0400

    Basic static components

---
+++ frontend/src/components/footer/index.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+
+        <div>
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/eduardo-juliano-de-almeida-60a74698/" target="_blank" rel="noreferrer">Eduardo Juliano</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                        Evento promovido pela escola DevSuperior: <a href="https://www.linkedin.com/in/eduardo-juliano-de-almeida-60a74698/" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                </div>
+            </footer>
+        </div>
+
+    );
+}
+
+export default Footer;
